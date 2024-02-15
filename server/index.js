6e513e44cdd734eca8fe66aa8756b6a205ad1bf4
unknown <lautycaceresss871@gmail.com>
2024-02-14 21:43:02 -0300

    d

--- server/index.js
+++ server/index.js
@@ -10,11 +10,17 @@ const client = new MercadoPagoConfig({
 const app = express();
 const port = 3000;
 
-app.use(cors({
-    origin: ['https://csmayoristas.com.ar', 'https://cs-server-gamma.vercel.app'],
-    methods: 'GET,POST',
-    optionsSuccessStatus: 200
-}));
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "React app URL"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
