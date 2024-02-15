02f27f49828042c03acb13e0de866aaf097033ba
unknown <lautycaceresss871@gmail.com>
2024-02-14 21:50:56 -0300

    s

--- server/index.js
+++ server/index.js
@@ -10,15 +10,7 @@ const client = new MercadoPagoConfig({
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "React app URL"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
+
 
 app.use(cors());
 app.use(express.json());
