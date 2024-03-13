bfcbb87fe76f3930797351c7ba59108b801408de
unknown <lautycaceresss871@gmail.com>
2024-03-13 09:37:24 -0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Hero from './pages/hero/Hero'
-// RRD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CartContent from "./components/CartContent/CartContent";
 import DataProvider from "./components/Context/DataContext";
 import Contacto from './pages/hero/contacto/Contacto';
@@ -14,30 +13,28 @@ import Pending from "./pages/hero/Payments/Pending";
 import Err404 from './pages/hero/Err404';
 import PoliticaPrivacidad from "./pages/popri/PoliticaPrivacidad";
 
-
 function App() {
-
   return (
-    
     <DataProvider> 
       <BrowserRouter>
-            <Navbar />
-            
-              <Routes>
-                <Route path="/" element={<Hero />}></Route>
-                <Route path='/carrito' element={<CartContent />}/>
-                <Route path='/contacto' element={<Contacto />}/>
-                <Route path='/pagoconfirmado' element={<Success />}/>
-                <Route path='/pagoconerror' element={<Failure />}/>
-                <Route path='/pagoenespera' element={<Pending />}/>
-                <Route path='/politicaprivacidad' element={<PoliticaPrivacidad />}/>
-                <Route path='*' element={<Err404 />}/>
-              </Routes>
-              <Whatsapp />
-            <Footer />
+          <Navbar />
+          <Whatsapp />
+        <Routes>
+
+            <Route path="/" element={<Hero />} />
+            <Route path='/carrito' element={<CartContent />} />
+            <Route path='/contacto' element={<Contacto />} />
+            <Route path='/pagoconfirmado' element={<Success />} />
+            <Route path='/pagoconerror' element={<Failure />} />
+            <Route path='/pagoenespera' element={<Pending />} />
+            <Route path='/politicaprivacidad' element={<PoliticaPrivacidad />} />
+            <Route path='*' element={<Err404 />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </DataProvider>
   )
 }
 
-export default App
+
+export default App;
